c3d8072898de7363cc0bee2598bd7d9e57836526
gyustur0409 <wnsgud0312@gmail.com>
2023-10-30 14:13:01 +0900

    green

--- Assets/scripts/GreenPattern.cs
+++ Assets/scripts/GreenPattern.cs
@@ -9,55 +9,37 @@ public class GreenPattern : MonoBehaviour
     GameObject[] area;
     GameObject player;
     Rigidbody rigid;
-    Vector3 moveVec;
-    bool area1, area2;
+    Vector3 currentVec;
+    bool area1, area2, lookAtPlayer, run;
+    Quaternion rotGoal;
 
     void Awake()
     {
         area = new GameObject[2];
-        area[0] = GameObject.Find("Tongue_collider");
+        area[0] = GameObject.Find("Hand_collider");
         area[1] = GameObject.Find("Head_collider");
         area[0].SetActive(false);
         area[1].SetActive(false);
         anim = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player");
         rigid = GetComponent<Rigidbody>();
+        state = 0;
         area1 = false;
         area2 = false;
-        choosPattern();
+        lookAtPlayer = false;
+        run = false;
+        choosePattern();
     }
 
-    IEnumerator attack1()
+    IEnumerator state_0()
     {
-        anim.SetTrigger("Attack1");
-        area[0].SetActive(true);
-        yield return new WaitForSeconds(3f);
-        area[0].SetActive(false);
-        yield return new WaitForSeconds(2f);
-        choosPattern();
-    }
-
-    IEnumerator attack2()
-    {
-        anim.SetTrigger("Attack2");
-        area[1].SetActive(true);
-        yield return new WaitForSeconds(3f);
-        area[1].SetActive(false);
-        yield return new WaitForSeconds(2f);
-        choosPattern();
-    }
-
-    void Go()
-    {
-        choosPattern();
-    }
-    void choosPattern()
-    {
-        /*if (area1)
+        lookAtPlayer = true;
+        yield return new WaitForSeconds(1f);
+        if (area1)
         {
             if (area2)
             {
-                Random.Range(0, 3);
+                state = Random.Range(1, 3);
             }
             else
             {
@@ -67,36 +49,96 @@ public class GreenPattern : MonoBehaviour
         else
         {
             state = 3;
-        }*/
-        state = Random.Range(0, 2);
+        }
+        lookAtPlayer = false;
+        choosePattern();
+    }
+    IEnumerator attack1()
+    {
+        anim.SetTrigger("Attack1");
+        area[0].SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        area[0].SetActive(false);
+        state = 0;
+        choosePattern();
+    }
+
+    IEnumerator attack2()
+    {
+        anim.SetTrigger("Attack2");
+        area[1].SetActive(true);
+        yield return new WaitForSeconds(3f);
+        area[1].SetActive(false);
+        state = 0;
+        choosePattern();
+    }
+    IEnumerator charge()
+    {
+        anim.SetTrigger("scream");
+        yield return new WaitForSeconds(1.4f);
+        anim.SetInteger("run", 1);
+        run = true;
+        lookAtPlayer = true;
+        currentVec = new Vector3(player.transform.position.x - transform.position.x, 0f, player.transform.position.z - transform.position.z);
+        yield return new WaitForSeconds(0.25f);
+        currentVec = new Vector3(player.transform.position.x - transform.position.x, 0f, player.transform.position.z - transform.position.z);
+        yield return new WaitForSeconds(0.25f);
+        currentVec = new Vector3(player.transform.position.x - transform.position.x, 0f, player.transform.position.z - transform.position.z);
+        yield return new WaitForSeconds(0.25f);
+        currentVec = new Vector3(player.transform.position.x - transform.position.x, 0f, player.transform.position.z - transform.position.z);
+        yield return new WaitForSeconds(0.25f);
+        currentVec = new Vector3(player.transform.position.x - transform.position.x, 0f, player.transform.position.z - transform.position.z);
+        yield return new WaitForSeconds(0.25f);
+        currentVec = new Vector3(player.transform.position.x - transform.position.x, 0f, player.transform.position.z - transform.position.z);
+        yield return new WaitForSeconds(0.25f);
+        currentVec = new Vector3(player.transform.position.x - transform.position.x, 0f, player.transform.position.z - transform.position.z);
+        lookAtPlayer = false;
+        yield return new WaitForSeconds(0.5f);
+        anim.SetInteger("run", 0);
+        run = false;
+        state = 0;
+        choosePattern();
+    }
+    IEnumerator movetime()
+    {//이동시 2초만 이동
+        yield return new WaitForSeconds(2f);
+        state = 0;
+        choosePattern();
+    }
+    void choosePattern()
+    {
+        Debug.Log("츄즈패턴");
         switch (state)
         {
             case 0:
-                StartCoroutine("attack1");
-                Debug.Log("아");
+                StartCoroutine("state_0");
                 break;
             case 1:
-                StartCoroutine("attack2");
+                StartCoroutine("attack1");//attack1 손으로 공격
                 break;
-            /*case 2:
-                Go();
+            case 2:
+                StartCoroutine("attack2");//attack2 꺠물기 
                 break;
             case 3:
-                Go();
+                StartCoroutine("charge");//charge 돌진 공격
                 break;
             case 4:
-                break;*/
+                StartCoroutine("movetime");
+                anim.SetInteger("walk", 1);
+                break;
+            case 5:
+                break;
             default:
                 break;
         }
     }
     void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.name == "Area1")
+        if (col.gameObject.name == "Area1")//바깥원
         {
             area1 = true;
         }
-        if (col.gameObject.name == "Area2")
+        if (col.gameObject.name == "Area2")//안쪽원
         {
             area2 = true;
         }
@@ -114,11 +156,39 @@ public class GreenPattern : MonoBehaviour
     }
     void Update()
     {
-        Vector3 dir = new Vector3(player.transform.position.x - transform.position.x, 0f, player.transform.position.y - transform.position.y);
+        Debug.Log(area2);
+        Debug.Log(state);
+        Vector3 dir = new Vector3(player.transform.position.x - transform.position.x, 0f, player.transform.position.z - transform.position.z);
+        Vector3 zero = new Vector3(0f, 0f, 0f);
+
+        if (run)
+        {
+            rigid.velocity = currentVec.normalized * 20.0f;
+        }
+
+        if (lookAtPlayer)
+        {
+            rotGoal = Quaternion.LookRotation(dir.normalized);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotGoal, 0.01f);
+        }
+
         if (state == 4)
         {
-            transform.rotation = Quaternion.LookRotation(dir);
+            lookAtPlayer = true;
+            if (area2 == true)
+            {
+                anim.SetInteger("walk", 0);
+                StopCoroutine("movetime");
+                lookAtPlayer = false;
+                state = 0;
+                choosePattern();
+            }
             rigid.velocity = dir.normalized * 4.0f;
+
+        }
+        else if (state != 3)
+        {
+            rigid.velocity = zero * 4.0f;
         }
     }
 }
--- Assets/scripts/Playermove.cs
+++ Assets/scripts/Playermove.cs
@@ -9,11 +9,11 @@ public class Playermove : MonoBehaviour
     Animator anim;
     float xInput, zInput, pSpeed, slashTime, slashCurrentTime, rollTime, rollCurrentTime;
     Vector3 moveVec, point;
+    Vector2 turn;
     Rigidbody pRigid;
     bool isSlashing, isRoll;
     void Start()
     {
-        camera = GameObject.FindGameObjectWithTag("MainCamera");
         pRigid = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         pSpeed = 4.0f;
@@ -23,17 +23,19 @@ public class Playermove : MonoBehaviour
         rollTime = 1.2f;
         isRoll = false;
     }
-
+    IEnumerator rolling()
+    {
+        //transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+        yield return new WaitForSeconds(1.15f);
+        //transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+    }
     void Update()
     {
-        camera.transform.position = new Vector3(transform.position.x, 12f, transform.position.z - 8f);
-        Vector3 mousePos = Input.mousePosition;
+        turn.x += Input.GetAxis("Mouse X");
+        transform.localRotation = Quaternion.Euler(0, turn.x, 0);
         xInput = Input.GetAxisRaw("Horizontal");
         zInput = Input.GetAxisRaw("Vertical");
         moveVec = new Vector3(xInput, 0f, zInput);
-        pRigid.velocity = moveVec.normalized * pSpeed;
-        Vector3 dir = new Vector3(mousePos.x - Screen.width / 2, 0f, mousePos.y - Screen.height / 2);
-        transform.rotation = Quaternion.LookRotation(dir);
         slashCurrentTime += Time.deltaTime;
         rollCurrentTime += Time.deltaTime;
         if (slashCurrentTime > slashTime)
@@ -48,29 +50,41 @@ public class Playermove : MonoBehaviour
         {
             isRoll = false;
         }
+        if (Input.GetKey("w"))
+        {
+            pRigid.velocity = transform.forward * pSpeed;
+        }
+        if (Input.GetKey("s"))
+        {
+            pRigid.velocity = -transform.forward * pSpeed;
+        }
+        if (Input.GetKey("d"))
+        {
+            pRigid.velocity = transform.right * pSpeed;
+        }
+        if (Input.GetKey("a"))
+        {
+            pRigid.velocity = -transform.right * pSpeed;
+        }
         if (Input.GetMouseButton(0) && !isSlashing && !isRoll)
         {
             slashCurrentTime = 0;
             isSlashing = true;
-            slashNum = Random.Range(1, 4);
+            slashNum = Random.Range(1, 3);
             if (slashNum == 1)
             {
-                slashTime = 1.2f;
+                slashTime = 1.2f / 1.5f;
                 anim.SetTrigger("slashing1");
             }
             else if (slashNum == 2)
             {
-                slashTime = 1.8f;
+                slashTime = 1.8f / 1.5f;
                 anim.SetTrigger("slashing2");
-            }
-            else if (slashNum == 3)
-            {
-                slashTime = 1.82f;
-                anim.SetTrigger("slashing3");
             }
         }
         else if (Input.GetKeyDown("left shift") && !isSlashing && !isRoll)
         {
+            StartCoroutine("rolling");
             rollCurrentTime = 0;
             isRoll = true;
             pSpeed = 10f;
