0d017ff0656d72d325a5c30982a698c0f274d5bb
qwd00 <129277903+qwd00@users.noreply.github.com>
2023-12-20 17:00:29 +0900

    v1.0

--- Assets/scripts/RedPattern.cs
+++ Assets/scripts/RedPattern.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class RedPattern : MonoBehaviour
 {
-    int state;
+    public static bool isAttacking;
+    public static int state;
+    public static int monsterHealth;
     Animator anim;
     GameObject[] area;
     GameObject player;
     Rigidbody rigid;
     Vector3 moveVec;
-    bool area1, area2;
+    bool area1, area2, lookAtPlayer, run, Last_ditch;
 
     void Awake()
     {
+        isAttacking = false;
+        monsterHealth = 1000;
         area = new GameObject[5];
         area[0] = GameObject.Find("Head_Collider");
         area[1] = GameObject.Find("Tail_Collider1");
@@ -28,9 +32,43 @@ public class RedPattern : MonoBehaviour
         anim = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player");
         rigid = GetComponent<Rigidbody>();
+        state = 0;
         area1 = false;
         area2 = false;
+        Last_ditch = false;
         choosPattern();
+    }
+    IEnumerator state_0()
+    {
+        float time;
+        yield return new WaitForSeconds(0.2f);
+        lookAtPlayer = true;
+        time = Random.Range(0f, 0.8f);
+        yield return new WaitForSeconds(time);
+        if (monsterHealth > 50 || Last_ditch)
+        {
+            if (area1)
+            {
+                if (area2)
+                {
+                    state = Random.Range(1, 3);
+                }
+                else
+                {
+                    state = 5;
+                }
+            }
+            else
+            {
+                state = 3;
+            }
+        }
+        else
+        {
+            state = 4;
+        }
+        lookAtPlayer = false;
+        choosePattern();
     }
 
     IEnumerator attack1()
@@ -63,45 +101,40 @@ public class RedPattern : MonoBehaviour
         choosPattern();
     }
 
+    IEnumerator attack4()
+    {
+        anim.SetTrigger("Take Off");
+        area[1].SetActive(true);
+        yield return new WaitForSeconds(3f);
+        area[1].SetActive(false);
+        yield return new WaitForSeconds(2f);
+        Last_ditch = true;
+        choosPattern();
+    }
+
     void Go()
     {
         choosPattern();
     }
     void choosPattern()
     {
-        /*if (area1)
-        {
-            if (area2)
-            {
-                Random.Range(0, 3);
-            }
-            else
-            {
-                state = 4;
-            }
-        }
-        else
-        {
-            state = 3;
-        }*/
-        state = Random.Range(0, 2);
+        state = Random.Range(0, 4);
         switch (state)
         {
             case 0:
-                StartCoroutine("attack1");
-                Debug.Log("아");
+                StartCoroutine("state_0");
                 break;
             case 1:
+                StartCoroutine("attack1");
+                break;
+            case 2:
                 StartCoroutine("attack2");
                 break;
-            /*case 2:
-                Go();
-                break;
             case 3:
-                Go();
+                StartCoroutine("attack3");
                 break;
             case 4:
-                break;*/
+                StartCoroutine("attack4");
             default:
                 break;
         }
